8142a597ec5d77b941967d0e5ec39138c11a2274
jc <jc@MacBook-Pro-de-jc.local>
2019-01-03 19:44:53 +0100

    Update branch

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 import { get } from "https";
 import { resolve } from "path";
 import fetch from "node-fetch";
-
+import { decode } from "bencode";
 console.log('Starting', encodeURI);
 
 const root = resolve('.');
@@ -12,15 +12,29 @@ const test = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Si
 const test2 = readFileSync(root + '/torrent-files/pic.torrent');
 
 const torrent = parseTorrent(test2);
-
+const torrent2 = decode(test2, undefined, undefined, 'utf8');
+console.log(torrent2);
 
 if (torrent.announce) {
+  var lo = '';
   console.log(torrent.announce, torrent.infoHash);
-  var params = encodeURIComponent(`${torrent.infoHash}`);
-  console.log(params, `${torrent.announce[0]}?info_hash=${params}`);
-  fetch(`${torrent.announce[0]}`).then((res) => {
-    return res.text();
-  }).then((res) => {
+  if (torrent.infoHash) {
+    torrent.infoHash = torrent.infoHash.toUpperCase();
+  } 
+  if (torrent.infoHashBuffer) {
+    var d = Buffer.from(torrent.infoHashBuffer, 10)
+    console.log('buffer  ? ',torrent.infoHashBuffer, torrent.infoHashBuffer.toString('base64'));
+    lo = escape('\xfd\xa7\xa2<X\xec\x05#\x81\x10\x05x\xa5\n\x00\xd7r\xa4(<');
+  }
+  var params = encodeURI(`${torrent.infoHash}`);
+  var peer_id = '12345678909876543210'
+  var url = `${torrent.announce[0]}?info_hash=${lo}`;
+  console.log(url);
+  fetch(url).then((res) => {
+    console.log(res);
+    return res.arrayBuffer();
+  }).then((res:any) => {
+    console.log(decode(res));
     console.log(res);
   }).catch(console.warn);
 }
